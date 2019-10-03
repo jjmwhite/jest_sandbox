45234c01cbc5990d2893ab55413c9b94be8efd6c
Jessica White <jessica.jm.white@gmail.com>
2019-10-03 13:42:44 -0400

    add testing block wrappers and readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Jest Testing Sandbox
-- Initial testing examples drawn from [Traversy Media](https://www.youtube.com/watch?v=7r4xVDI2vho)
+Initial testing examples drawn from [Traversy Media](https://www.youtube.com/watch?v=7r4xVDI2vho)
+
+To use:
+- Clone this repo
+- Run `npm install`
+- Add / modify code
+- Testing scripts
+    - Single run of test suite: `npm run test`
+    - Live updates: `npm run test-watch`
--- functions.test.js
+++ functions.test.js
@@ -1,25 +1,30 @@
 const functions = require('./functions');
 
-const initDB = () => console.log('database initialized...');
-const closeDB = () => console.log('database closed...');
+// You can set the #Each and #All functions within a describe block to affect testing scope
+const initDB = () => console.log('Database initialized...');
+const closeDB = () => console.log('Database closed...');
 const startTask = () => console.log('Starting new task');
 const endTask = () => console.log('Ending task');
 
+// NB: In the test below, the #All test only within the describe block (weird, right?)
 beforeAll(() => initDB());
 afterAll(() => closeDB());
 
-beforeEach(() => startTask());
-afterEach(() => endTask());
-
-test('adds two numbers', () => {
-    expect(functions.add(2,3)).toBe(5);
-    expect(functions.add(-1,1)).toBe(0);
+describe('it should perform tasks between each test', () => {
+    beforeEach(() => startTask());
+    afterEach(() => endTask());
+    
+    test('adds two numbers', () => {
+        expect(functions.add(2,3)).toBe(5);
+        expect(functions.add(-1,1)).toBe(0);
+    });
+    
+    test('divides two numbers', () => {
+        expect(functions.divide(4,2)).toBe(2);
+    });
 });
 
-test('divides two numbers', () => {
-    expect(functions.divide(4,2)).toBe(2);
-});
-
+// can use test() or it() descriptive syntax
 test('User should have a first and last name', () => {
     const user = functions.createUser();
     expect(user.firstName).not.toBeFalsy;
@@ -31,4 +36,3 @@ test('User test name should be Ervin Howell', () => {
     return functions.fetchUser()
         .then( data => { expect(data.name).toEqual('Ervin Howell') });
 });
-
